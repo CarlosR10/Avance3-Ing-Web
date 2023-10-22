bc60bc8038bb701733506e5b032720df22e1dcf1
Johans1047 <jonyalex1047@gmail.com>
2023-10-22 01:59:42 -0500

    Header, Footer y Formulario Responsive (Funciona para todos los formularos de reserva)

--- js/componentes.js
+++ js/componentes.js
@@ -1,5 +1,19 @@
 $(document).ready(function() { 
-    $("#nav-bar").load("componentes/nav-bar.html", {
+    $("#header").load("componentes/header.html", {
     method: "GET"
     });
+});
+
+$(document).ready(function() { 
+    $("#footer").load("componentes/footer.html", {
+    method: "GET"
+    });
+});
+
+const menu = document.querySelector('#collapse');
+
+menu.addEventListener('click', () => {
+  if (menu.classList.contains('show')) {
+    menu.classList.remove('show');
+  }
 });
